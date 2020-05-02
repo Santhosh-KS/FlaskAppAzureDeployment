c845bb623dd2575e44dfcfc43feb3ad439a63953
Santhosh K S <santhoshachar08@gmail.com>
2020-05-02 21:02:38 +0200

    creating post boot up script for Azure

---
+++ scripts/postbuild.sh
@@ -0,0 +1,7 @@
+echo "Running install script"
+pip install wheel gunicorn
+python setup.py bdist_wheel
+cd dist
+pip install --no-cache-dir flask_pytorch_web_app-0.0.0-py3-none-any.whl
+cd -
+echo "Done installation "
