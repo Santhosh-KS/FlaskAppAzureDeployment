eccd64a47daca5790f65968353bc894f4d8734cd
Santhosh K S <santhoshachar08@gmail.com>
2020-05-02 18:51:15 +0200

    azure trials

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 # start.sh
 
 export FLASK_APP=wsgi.py
-export FLASK_DEBUG=1
+export FLASK_DEBUG=0
 export APP_CONFIG_FILE=config.py
 export UPLOADED_PHOTOS_DEST=/tmp/images/
 export SECRET_KEY="abcd"
--- wsgi.py
+++ wsgi.py
@@ -5,4 +5,5 @@ app = create_app()
 
 #TODO don't forget to remove debug option during deployment
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', debug=True)
+    app.run()
+    #app.run(host='0.0.0.0', debug=True)
