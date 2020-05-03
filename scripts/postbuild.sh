c38a7b95c8592e14bdf55056c42cb408532be6fe
Santhosh K S <santhoshachar08@gmail.com>
2020-05-03 15:11:17 +0200

    req test

--- scripts/postbuild.sh
+++ scripts/postbuild.sh
@@ -1,18 +1,19 @@
-echo "Running install script"
-pip install wheel gunicorn
-python setup.py bdist_wheel
-cd dist
-pip install --no-cache-dir flask_pytorch_web_app-0.0.0-py3-none-any.whl
-cd -
-mkdir -p /tmp/images
-echo "Installing Flask-Uploads"
-pip install Flask-Uploads
-echo "Done installation "
+#echo "Running install script"
+#pip install wheel gunicorn
+#python setup.py bdist_wheel
+#cd dist
+#pip install --no-cache-dir flask_pytorch_web_app-0.0.0-py3-none-any.whl
+#cd -
+#mkdir -p /tmp/images
+#echo "Installing Flask-Uploads"
+#pip install Flask-Uploads
+#echo "Done installation "
+#pwd
+#ls
+##which python
+#python --version
+#ls /home/site/wwwroot
+#pip freeze
+pip install --no-cache-dir -r  requirements.txt
 echo "HERE are exports"
 export
-pwd
-ls
-which python
-python --version
-ls /home/site/wwwroot
-pip freeze
