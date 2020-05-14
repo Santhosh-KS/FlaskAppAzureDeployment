12875fd6035434fa9b129772a2777ad9aabecc10
Santhosh K S <santhoshachar08@gmail.com>
2020-05-14 13:34:02 +0200

    Local build steps + Debug procedure for App service

--- local_run.sh
+++ local_run.sh
@@ -5,6 +5,6 @@ export FLASK_DEBUG=0
 export APP_CONFIG_FILE=config.py
 export UPLOADED_PHOTOS_DEST=/tmp/images/
 export SECRET_KEY="abcd"
-gunicorn --bind 0.0.0.0:80 -w 4 wsgi:app
+gunicorn --bind 0.0.0.0 -w 4 wsgi:app
 #uwsgi --http-socket :5000 --plugin python36 --module wsgi:app  --virtualenv  /home/l-8053/py3
 #uwsgi myapp.ini
